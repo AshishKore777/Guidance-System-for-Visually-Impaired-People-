e53761ea7a78b0c5480f8fb82a1f798726c900be
AshishKore777 <53338065+AshishKore777@users.noreply.github.com>
2020-04-25 04:49:25 +0530

    Second Commit

--- Runner.py
+++ Runner.py
@@ -1,75 +1,84 @@
-from flask import Flask, render_template, url_for, request, redirect, Response, stream_with_context
-import time
+"""
+    Title: Guidance-System-for-Visually-Impaired-People
+    Author: Ashish Kore and Syeda Atiya Husain and Nupur Kshatri
+    Language: Python
+    Requirements:
+    Python version-> 3 or later
+    Python Packages or Modules->    1. flask
+                                    2. imageai
+                                    3. requests
+                                    4. cv2
+                                    5. numpy
+                                    6. random
+                                    7. gtts
+                                    8. playsound
+    Additional Files->      1. Yolov3.h5
+"""
+
+#importing necessary packages and modules
+from flask import Flask, render_template
 from imageai.Detection import ObjectDetection
 import requests as r
 import cv2
 import numpy as np
 import random
 from gtts import gTTS 
-import os
-
 import playsound
+
+#initialising flask based app
 app = Flask(__name__)
 
 
 @app.route('/')
+#This function will be called on landing up to the above endpoint.
 def home():
+"""
+This function returns a template named home.html.
+"""
     return render_template('home.html')
 
-
-# def audio_response():
-# #     l=["dog"]
-#     for x in range(2):
-# #         s="static/audio/{}.mp3".format(l[x])
-#         with open("static/audio/bark.mp3", "rb") as aud:
-#             data = aud.read(1024)
-#             while data:
-#                 yield data
-#                 data = aud.read(1024)
-            
-#         time.sleep(5.0)
-# #     return redirect('/index.html')
-
-
-# @app.route('/audio')
-# def audio():
-#     return Response(audio_response(), mimetype='audio/x-mp3')
-
 class main():
+"""
+This class is defined to instantiate ObjectDetection class and load Yolov3 model through the constructor.
+"""
     def __init__(self):
-        
-        self.detector = ObjectDetection()
-        self.model_path = "static/yolo.h5"      #actual path
-
-        self.detector.setModelTypeAsYOLOv3()
-        self.detector.setModelPath(self.model_path)
-        self.detector.loadModel()
-        
-url="http://192.168.43.225:8080/shot.jpg" #ip webcam url        
+        self.detector = ObjectDetection() #creating object of Object Detection class.
+        self.model_path = "static/yolo.h5" #specifying path of yolo model.
+        self.detector.setModelTypeAsYOLOv3() #selecting model type
+        self.detector.setModelPath(self.model_path) #setting model path
+        self.detector.loadModel()#loading Yolo model
+      
+url="http://192.168.43.225:8080/shot.jpg" # url for IP Web Camera App server       
     
         
 @app.route('/index', methods=['POST'])
+#This function will be called on landing up to the above endpoint.
 def index_func():
+"""
+This function upon calling performs object detection task on images obtained from IP Web Camera. 
+There after it generates audio instructions containing names of the object classes.
+This process gets continued on live images untill the user shuts down the IP Web Camera app.
+"""
     try:
-
-        s=main()
+        s=main()#initializing main class object
         z=0
         while True:
 
-            res= r.get(url)
+            res= r.get(url)# hits the IP Web Camera server url for image response 
             arr=np.array(bytearray(res.content), dtype=np.uint8)
             img=cv2.imdecode(arr,-1)
-#             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            cv2.imwrite("static/detectionimage/a{}.jpeg".format(z),img)
+            cv2.imwrite("static/detectionimage/a{}.jpeg".format(z),img)#saving the response image in jpeg format
 
 
             input_path = "static/detectionimage/a{}.jpeg".format(z)
             output_path = "static/output/a{}.jpeg".format(z)
+            # calling detection function for object detection and storing the object names in a list
             detection = s.detector.detectObjectsFromImage(input_image=input_path, output_image_path=output_path)
             a=""
             obj=set()
             for d in detection:
                 obj.add(d['name'])
+            #generating audio instructions according to objects name list
             language = 'en'
             if len(obj)==0:
                 sss="It seems to be nothing on your way"
@@ -89,13 +98,11 @@ def index_func():
                 elif x==3:
                     speech = gTTS(text = ss3, lang = language, slow = False)
 
-            speech.save("static/audio/a{}.mp3".format(z))
-            playsound.playsound("static/audio/a{}.mp3".format(z),True)
-    #         time.sleep(5)
-    #         os.remove("static/audio/a{}.mp3".format(z))
-            z+=1
+            speech.save("static/audio/a{}.mp3".format(z))#saving generated audio
+            playsound.playsound("static/audio/a{}.mp3".format(z),True)#playing generated audio
+
     except:
-            return render_template('output.html')
+            return render_template('output.html')#returns output.html template when the app is closed
 
 
 if __name__ == '__main__':
